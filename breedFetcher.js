c860f0b0cdca2a39113840abc6592d2f6c81a080
Anto Ter-Mesropian <antotm99@gmail.com>
2021-01-14 21:36:17 +0000

    added mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,25 @@
 const request = require('request');
-const args = process.argv.splice(2);
+const fetchBreedDescription = function(breedName, callback) {
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, resp, body) => {
-  //console.log(body);
-  //console.log(typeof body);
+
+    if (error) {
+      callback(`${error}`,null);
+      return;
+    }
 
   
-  try {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
+    try {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
 
-  } catch(error) {
+    } catch (error) {
 
-    console.log(`The breed ${args} does not exist.`);
-  }
-})
+      callback(`The breed ${breedName} does not exist.`, null);
+    }
+  });
 
+};
+
+module.exports = {fetchBreedDescription};
